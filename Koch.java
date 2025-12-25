aa7c8e90c8abc10fe0365dc3bffd8f5d19e7840b
Maya Sapir <maya.sapir@post.runi.ac.il>
2025-12-26 00:05:25 +0200

    Completed assignment

--- Koch.java
+++ Koch.java
@@ -22,21 +22,54 @@ public class Koch {
 		// Gets n, and draws a Koch snowflake of n edges in the standard canvass.
 		snowFlake(Integer.parseInt(args[0]));
 		*/
+		snowFlake(Integer.parseInt(args[0]));
 	}
 
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
+		if (n == 0) {
 		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		return;
+		}
+		// Points that divide the segment into thirds
+        double xLeft  = x1 + (x2 - x1) / 3.0;
+        double yLeft  = y1 + (y2 - y1) / 3.0;
+
+        double xRight = x2 - (x2 - x1) / 3.0;
+        double yRight = y2 - (y2 - y1) / 3.0;
+
+		// Midpoint of the middle segment
+        double midX = (xLeft + xRight) / 2.0;
+        double midY = (yLeft + yRight) / 2.0;
+
+		double offset = Math.sqrt(3) / 6.0;
+        double xPeak = midX + offset * (yLeft - yRight);
+        double yPeak = midY + offset * (xRight - xLeft);
+
+		// Recursive calls on the 4 new segments
+        curve(n - 1, x1, y1, xLeft, yLeft);
+        curve(n - 1, xLeft, yLeft, xPeak, yPeak);
+        curve(n - 1, xPeak, yPeak, xRight, yRight);
+        curve(n - 1, xRight, yRight, x2, y2);
+
+		
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
 	public static void snowFlake(int n) {
+		StdDraw.clear(StdDraw.WHITE);
+        StdDraw.setPenColor(StdDraw.BLACK);
 		// A little tweak that makes the drawing look better
-		StdDraw.setYscale(0, 1.1);
-		StdDraw.setXscale(0, 1.1);
+		StdDraw.setYscale(-0.2, 1.2);
+		StdDraw.setXscale(-0.2, 1.2);
 		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+		double x1 = 0.1, y1 = 0.1;
+        double x2 = 1.0, y2 = 0.1;
+        double x3 = 0.55, y3 = 0.1 + 0.9 * (Math.sqrt(3) / 2.0);
+
+		curve(n, x1, y1, x2, y2);
+        curve(n, x2, y2, x3, y3);
+        curve(n, x3, y3, x1, y1); 
 	}
 }
